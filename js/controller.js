dee72b131d1813c31e0dffdfed132922510146a7
harshpatel7962 <harshpatel7962@gmail.com>
2016-02-24 19:41:15 +0530

    first commit

---
+++ js/controller.js
@@ -0,0 +1,19 @@
+myapp.controller("basicUiTree",['$scope',function($scope){
+    $scope.data = [{
+      'id': 1,
+      'title': 'textbox',
+      'nodes': []
+    }, {
+      'id': 2,
+      'title': 'textarea',
+      'nodes': []
+    }, {
+      'id': 3,
+      'title': 'checkbox',
+      'nodes': []
+    }, {
+      'id': 4,
+      'title': 'radiobutton',
+      'nodes': []
+    }];
+}]);
